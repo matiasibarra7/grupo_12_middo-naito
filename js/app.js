db7303e45b61ab7f5d8336a73fa596406789d894
Matias <matiasibarra7@gmail.com>
2020-06-30 11:32:35 -0300

    cambios a la organización de las carpetas

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-
-app.get('/', (req, res) => {
-    res.sendFile(`${path.normalize(__dirname + '/..')}/index.html`)
-})
-
-app.listen(3000)
---
+++ public/js/script.js
@@ -0,0 +1,1 @@
+let variable = 145
