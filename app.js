a734f29c0a987a1de380ce56595d3a651aface54
matiasibarra7 <matiasibarra7@gmail.com>
2020-06-30 22:25:29 -0300

    prueba con algunas fuentes

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require('express')
-const { static } = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`)
-})
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
 
-app.listen(3000)
+app.listen(3000);
