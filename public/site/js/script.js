1829f13a8c38eaa29accebe67897ff96a3140844
Matias <matiasibarra7@gmail.com>
2020-07-21 16:01:54 -0300

    añadido el boton desplegable y un logo medio aceptable

--- public/site/js/script.js
+++ public/site/js/script.js
@@ -1,1 +1,9 @@
 let variable = 145
+
+function openSidebar() {
+    document.querySelector(".sidebar").style.display = "block"
+}
+
+function closeSidebar() {
+    document.querySelector(".sidebar").style.display = "none"
+}
