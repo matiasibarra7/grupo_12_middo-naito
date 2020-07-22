0b4a2352e2bce7e91b318bec89f7edc7942fa8a0
Matias <matiasibarra7@gmail.com>
2020-07-22 01:05:10 -0300

    Algunos cambios al sidebar y a algunos estilos generales

--- public/site/js/script.js
+++ public/site/js/script.js
@@ -1,9 +1,11 @@
 let variable = 145
 
 function openSidebar() {
-    document.querySelector(".sidebar").style.display = "block"
+    document.querySelector(".sidebar").style.display = "block";
+    document.querySelector(".close-zone").style.display = "block";
 }
 
 function closeSidebar() {
-    document.querySelector(".sidebar").style.display = "none"
+    document.querySelector(".sidebar").style.display = "none";
+    document.querySelector(".close-zone").style.display = "none";
 }
