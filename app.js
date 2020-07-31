bb229e1cade8d5c20adc6d1a8f195076d13ef117
Matias <matiasibarra7@gmail.com>
2020-07-30 22:45:25 -0300

    Organizando las carpetas

--- app.js
+++ app.js
@@ -5,31 +5,31 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: false}))
 
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/public/site/html/index.html`);
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
 app.get("/cart", (req, res) => {
-  res.sendFile(`${__dirname}/public/site/html/productCart.html`);
+  res.sendFile(`${__dirname}/views/productCart.html`);
 });
 
 app.get("/us", (req, res) => {
-  res.sendFile(`${__dirname}/public/site/html/about-us.html`);
+  res.sendFile(`${__dirname}/views/about-us.html`);
 });
 
 app.get("/products", (req, res) => {
-  res.sendFile(`${__dirname}/public/site/html/products.html`);
+  res.sendFile(`${__dirname}/views/products.html`);
 });
 
 app.get("/details", (req, res) => {
-  res.sendFile(`${__dirname}/public/site/html/productDetail.html`);
+  res.sendFile(`${__dirname}/views/productDetail.html`);
 });
 
 app.get("/register", (req, res) => {
-  res.sendFile(`${__dirname}/public/site/html/register.html`);
+  res.sendFile(`${__dirname}/views/register.html`);
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(`${__dirname}/public/site/html/login.html`);
+  res.sendFile(`${__dirname}/views/login.html`);
 });
 
 app.listen(3000, () => {
