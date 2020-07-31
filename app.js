cebc8d1f4c054ceb9b7bdebb60ecd25acfa66cc7
Juanbarriera <juanbarriera.07@gmail.com>
2020-07-31 18:06:41 -0300

    Ruteo y controladores de users, products y main

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
 const express = require("express");
 const app = express();
+const mainRoutes = require('./routes/main');
+const productsRoutes = require('./routes/products');
+const userRoutes = require('./routes/users');
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: false}))
 
-app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/views/index.html`);
-});
 
-app.get("/cart", (req, res) => {
-  res.sendFile(`${__dirname}/views/productCart.html`);
-});
+app.use('/', mainRoutes);
+app.use('/products', productsRoutes);
+app.use('/users', userRoutes);
 
-app.get("/us", (req, res) => {
-  res.sendFile(`${__dirname}/views/about-us.html`);
-});
 
-app.get("/products", (req, res) => {
-  res.sendFile(`${__dirname}/views/products.html`);
-});
-
-app.get("/details", (req, res) => {
-  res.sendFile(`${__dirname}/views/productDetail.html`);
-});
-
-app.get("/register", (req, res) => {
-  res.sendFile(`${__dirname}/views/register.html`);
-});
-
-app.get("/login", (req, res) => {
-  res.sendFile(`${__dirname}/views/login.html`);
-});
 
 app.listen(3000, () => {
   console.log("Servidor escuchando en el puerto 3000");
