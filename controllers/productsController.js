a04bf132b113b3fc3375213c21ac8e0539d67122
matiasibarra7 <matiasibarra7@gmail.com>
2020-08-18 18:41:51 -0300

    Agregado los primeros 4 pasos de bread

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,23 +1,41 @@
-const fs= require("fs");
+const fs = require("fs");
 const productsJson = fs.readFileSync(__dirname + "/../data/products.json");
-const productsData= JSON.parse(productsJson);
+const productsData = JSON.parse(productsJson);
+const path = require("path");
 
 const productsController = {
-    main: (req, res) => {
-        res.render('./products/products', {productsData});
-    },
-    details: (req, res) => {
-        res.render('./products/productDetail');
-    },
-    cart: (req, res) => {
-        res.render(`./products/productCart`, {productsData});
-    },
-    add: (req, res) => {
-        res.render(`./products/productAdd`);
-    },
-    edit: (req, res) => {
-        res.render(`./products/productEdit`);
-    }
-}
+  main: (req, res) => {
+    res.render("./products/products", { productsData });
+  },
+  details: (req, res) => {
+    let id = req.params.id;
+
+    res.render("./products/productDetail", { productData: productsData[id] });
+  },
+  cart: (req, res) => {
+    res.render(`./products/productCart`, { productsData });
+  },
+  add: (req, res) => {
+    res.render(`./products/productAdd`);
+  },
+  edit: (req, res) => {
+    res.render(`./products/productEdit`);
+  },
+  store: (req, res) => {
+    let id = productsData.pop().id;
+
+    let newProduct = req.body;
+    newProduct.id = ++id;
+    newProduct.alt = req.body.name;
+    newProduct.image = "imagen - " + path.basename(req.file.originalname);
+
+    let newProductList = JSON.parse(fs.readFileSync(__dirname + "/../data/products.json"));
+    newProductList.push(newProduct);
+
+    fs.writeFileSync(__dirname + "/../data/products.json", JSON.stringify(newProductList, null, " "));
+
+    res.redirect("/products");
+  },
+};
 
 module.exports = productsController;
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const productsController = require("../controllers/productsController");
+const path = require("path");
 
-router.get("/", productsController.main);
-router.get("/details", productsController.details);
+const multer = require("multer");
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, `${__dirname}/../public/images/products`);
+  },
+  filename: function (req, file, cb) {
+    cb(null, "imagen - " + path.basename(file.originalname));
+  },
+});
+
+upload = multer({ storage });
+
+router.get("/", productsController.main); //1 -- Listado de productos
+
+router.get("/add", productsController.add); //2 -- Formulario de creación de productos
+
+router.post("/add", upload.single("image"), productsController.store); //4 -- Acción de creación (a donde se envía el formulario)
+
+router.get("/details/:id", productsController.details); //3 -- Detalle de un producto particular
+
 router.get("/cart", productsController.cart);
-router.get("/add", productsController.add);
 router.get("/edit", productsController.edit);
 
 module.exports = router;
