d642565ecd7556a43fa08565c79a7841a2353125
Matias <matiasibarra7@gmail.com>
2020-08-24 16:30:46 -0300

    cambie btn-black a btn-default y agregué una clase red, pase los estilos desde el html al css de product-edit, corregi algunos desfasajes de las imagenes, entre otras cosas

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -13,7 +13,6 @@ function showAutoSlides() {
     slideAutoIndex++;
     if (slideAutoIndex > slides.length) {slideAutoIndex = 1}
     slides[slideAutoIndex-1].style.display = "block";
-    setTimeout(showAutoSlides, 4000); // Change image every 4 seconds
+    setTimeout(showAutoSlides, 8000); // Change image every 8 seconds
 }
-
 /* Fin Banner dinámico en home */
