f60418e35cad548c733fb0e543110fdeee24f128
santiagomartino <sanmb182@gmail.com>
2020-08-31 18:13:21 -0300

    Modificaciones en productDetails ejs, css y script.js

--- public/js/script.js
+++ public/js/script.js
@@ -9,3 +9,15 @@ function closeSidebar() {
     document.querySelector(".sidebar").style.display = "none";
     document.querySelector(".close-zone").style.display = "none";
 }
+
+
+
+function toggleSizeTable() {
+    let display = document.querySelector(".product-size-table").style.display;
+    if(display == "block"){
+        document.querySelector(".product-size-table").style.display = "none";
+    } else {
+        document.querySelector(".product-size-table").style.display = "block";
+    }
+}
+
