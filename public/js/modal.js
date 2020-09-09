f040e92221f3cf2d6533d9f3172ea4ba4a8c5681
Matias <matiasibarra7@gmail.com>
2020-09-09 16:02:27 -0300

    algunas modificaciones en el modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,20 +1,15 @@
-function newNote() {
+function toggleModal() {
   let divModal = document.querySelector(".modal-naito");
   let divCover = document.querySelector(".cover-modal");
 
-  divModal.style.display = "flex";
+  if (divModal.style.opacity == 0) {
+    divModal.style.opacity = 1;
+    divModal.style.visibility = "visible";
+    divCover.style.display = "block";
+  } else {
+    divModal.style.opacity = 0;
+    divModal.style.visibility = "hidden";
+    divCover.style.display = "none";
+  }
 
-  divModal.style.opacity = 1;
-  divModal.style.visibility = "visible";
-
-  divCover.style.display = "block";
-}
-  
-function closeModal() {
-  let divModal = document.querySelector(".modal-naito");
-  let divCover = document.querySelector(".cover-modal");
-
-  divModal.style.opacity = 0;
-  divModal.style.visibility = "hidden";
-  divCover.style.display = "none";
 }
