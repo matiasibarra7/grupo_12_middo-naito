9e48129975c5b4b44314354c85c93385bcb6bff1
Matias <matiasibarra7@gmail.com>
2020-09-10 02:34:45 -0300

    homogeneizados algunas clases en el header y limpiado un poco el script.js

--- public/js/script.js
+++ public/js/script.js
@@ -2,22 +2,12 @@
 function openSidebar() {
   document.querySelector(".sidebar").classList.add("showing-sidebar")
   document.querySelector(".close-zone").style.display = "block";
-  document.querySelector(".fas.fa-times").style.display = "block";
+  document.querySelector(".close-icon-sidebar").style.display = "flex";
 }
 
 function closeSidebar() {
   document.querySelector(".sidebar").classList.remove("showing-sidebar")
   document.querySelector(".close-zone").style.display = "none";
-  document.querySelector(".fas.fa-times").style.display = "none";
+  document.querySelector(".close-icon-sidebar").style.display = "none";
 }
-
-function toggleSizeTable() {
-  let display = document.querySelector(".product-size-table").style.display;
-  if(display == "block"){
-    document.querySelector(".product-size-table").style.display = "none";
-  } else {
-    document.querySelector(".product-size-table").style.display = "block";
-  }
-}
-
 /* Fin Barra de menú en mobile */
