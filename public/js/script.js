45178d6e4e0ed6f54b311281dd77eeffb67cdc4d
Matias <matiasibarra7@gmail.com>
2020-09-13 20:24:37 -0300

    Añadida la opcion de modificar la cantidad en el carrito desde front

--- public/js/script.js
+++ public/js/script.js
@@ -12,9 +12,22 @@ function closeSidebar() {
 }
 /* Fin Barra de menú en mobile */
 
-
+/* Función para mostrar el buscador al clickar */
 function toggleSearcher() {
   let divSerachField = document.querySelector(".searcher-field");
   divSerachField.classList.toggle("active")
 }
+/* Fin Función para mostrar el buscador al clickar */
 
+/* Función para cambiar la cantidad en el carrito */
+function changeQuantity(el) {
+  let spanValue = el.parentElement.children[1]
+  if (el.classList.contains("fa-plus-square")) {
+    spanValue.innerText = parseInt(spanValue.innerText) + 1
+  } else {
+    if (spanValue.innerText > 1) {
+      spanValue.innerText = parseInt(spanValue.innerText) - 1
+    }
+  }
+}
+/* Fin Función para cambiar la cantidad en el carrito */
