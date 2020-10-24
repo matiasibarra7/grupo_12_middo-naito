3131b20ac43bbb61d62be732b3d43152d912e679
Matias <matiasibarra7@gmail.com>
2020-10-24 20:14:30 -0300

    Añadida la primera versión del dashboard en la carpeta build y añadido serve como dependencia

---
+++ build/precache-manifest.c17001261a072355604c7b85a9b4e647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541bd91f34127e66ed9efa61e4b04a76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4184ec1d5d4c97dfa3c",
+    "url": "/static/js/2.032a6802.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.032a6802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad9cccd641015357892",
+    "url": "/static/js/main.9004d7b6.chunk.js"
+  },
+  {
+    "revision": "4da08649c8825dadac07",
+    "url": "/static/js/runtime-main.a13dccee.js"
+  }
+]);
